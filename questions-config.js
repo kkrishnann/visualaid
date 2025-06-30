1d3ea4ec4e3b6e04e06a6fcfb83ea87a816f04a7
kkrishnann <109610020+kkrishnann@users.noreply.github.com>
2025-06-30 19:06:03 -0400

    modified questions, footer

--- questions-config.js
+++ questions-config.js
@@ -2,13 +2,13 @@
 // Modify these questions to customize the application
 
 const DEFAULT_QUESTIONS = [
-    "What is your favorite color ?",
-    "What accomplishment are you most proud of?",
-    "What is one skill you've always wanted to learn but haven't had the chance to yet?",
-    "Who is your favorite person",
-    "What is something you're grateful for today, no matter how small it might seem?",
-    "What is one thing you would like to accomplish in the next year?",
-    "What is something you've learned about yourself recently?"
+    "Hi, Tell me about yourself",
+    "How do you deal with a difficult customer?",
+    "Why should we hire you?",
+    "Tell me about your family",
+    "Tell me about the city you live in?",
+    "What is an accomplishment that you are proud of?",
+    "What are your strengths?"
 ];
 
 // Export for use in questions-simple.js
